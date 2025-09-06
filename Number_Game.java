dfa16f47e4897fd27e1aa4633d6754d5f155c3f5
Rajnish Kumar <rajnishofficial011@gmail.com>
2025-09-07 02:09:07 +0530

    Task 1 completed

--- Number_Game.java
+++ Number_Game.java
@@ -1,5 +1,58 @@
 package CODSOFT;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Number_Game {
-    
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int maxAttempts = 10;   
+        int score = 0;        
+        boolean playAgain = true;
+
+        System.out.println("Number Guessing Game!");
+
+        while (playAgain) {
+            int numberToGuess = rand.nextInt(100) + 1; 
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("\nGuess a number between 1 to 100");
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess (" + (maxAttempts - attempts) + " attempts left): ");
+                int userGuess = sc.nextInt();
+                attempts++;
+
+                if (userGuess == numberToGuess) {
+                    System.out.println("Correct! You guessed it in " + attempts + " attempts.");
+                    score++;
+                    guessedCorrectly = true;
+                    break;
+                } else if (userGuess > numberToGuess) {
+                    System.out.println("📉 Too high! Try again.");
+                } else {
+                    System.out.println("📈 Too low! Try again.");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println(" Out of attempts! The correct number was: " + numberToGuess);
+            }
+
+            System.out.println("Your current score: " + score);
+
+            System.out.print("Do you want to play another round? (yes/no): ");
+            String response = sc.next().toLowerCase();
+            playAgain = response.equals("yes");
+        }
+
+        System.out.println("\n Final Score: " + score);
+        System.out.println("Thanks for playing!");
+        sc.close();
+    }
 }
